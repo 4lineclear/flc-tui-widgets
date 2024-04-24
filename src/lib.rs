ba7b4220cf7970990ae8ff1604b045f4a4cb4ad7
4lineclear <tamimhakimi21@gmail.com>
2024-04-23 22:12:11 -0400

    Added the base files.

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+//! Modified widgets the [Ratatui](https://ratatui.rs/) library.
+
+/// Custom widgets, mostly a mirror of ratatui's
+pub mod widgets;
---
+++ src/widgets.rs
@@ -0,0 +1,6 @@
+pub use paragraph::*;
+/// A paragraph that has different scroll behavior
+pub mod paragraph;
+
+/// Since the reflow module of ratatui is private, we just copy it here.
+mod reflow;
